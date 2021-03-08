e22cac486ea7f628e8ea4ecb5b2029db40545f29
Adrien Grandemange <itsmeadri@gmail.com>
2021-03-09 00:11:17 +0100

    First commit (runs on mac, compiles but crashes on android)

---
+++ src/main.rs
@@ -0,0 +1,15 @@
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::prelude::*;
+
+#[cfg(target_arch = "wasm32")]
+#[wasm_bindgen(start)]
+pub fn wasm_main() {
+    std::panic::set_hook(Box::new(console_error_panic_hook::hook));
+    main();
+}
+
+mod app;
+
+pub fn main() {
+    app::run_app();
+}
