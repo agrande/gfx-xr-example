fd0c41383c80f61236f973e91309bd6cbc9dc6ec
Adrien Grandemange <itsmeadri@gmail.com>
2021-03-10 00:33:30 +0100

    Simplify android boilerplate by using macro

--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,5 @@
 mod app;
-use ndk_glue;
-
-#[no_mangle]
-#[cfg(target_os = "android")]
-unsafe extern "C" fn ANativeActivity_onCreate(
-    activity: *mut std::os::raw::c_void,
-    saved_state: *mut std::os::raw::c_void,
-    saved_state_size: usize,
-) {
-    ndk_glue::init(
-        activity as _,
-        saved_state as _,
-        saved_state_size as _,
-        android_main,
-    );
-}
-
+#[cfg_attr(target_os = "android", ndk_glue::main(backtrace = "on"))]
 pub fn android_main() {
     app::run_app();
 }
